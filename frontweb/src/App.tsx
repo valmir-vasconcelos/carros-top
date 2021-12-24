32d9b66d01f4f0a1d71723c983c69f522a9d0162
Valmir Vasconcelos <valmir.bsi@gmail.com>
2021-12-23 23:57:20 -0300

    Navbar styling

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,9 +1,10 @@
 import './assets/styles/custom.scss';
 import './App.css';
+import Navbar from 'components/Navbar';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <Navbar/>
   );
 }
 
---
+++ frontweb/src/components/Navbar/index.tsx
@@ -0,0 +1,43 @@
+import './styles.css';
+import 'bootstrap/js/src/collapse.js';
+
+const Navbar = () => {
+  return (
+      <>
+    <nav className="navbar navbar-expand-md navbar-dark  bg-primary main-nav">
+      <div className='container-fluid'>
+        <a href="link" className="nav-logo-text">
+          <h4>Carros Top</h4>
+        </a>
+
+        <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#carrostop-navbar"
+            aria-controls="carrostop-navbar"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+            >
+            <span className="navbar-toggler-icon"></span>
+        </button>
+
+
+        <div className='collapse navbar-collapse' id="carrostop-navbar">
+          <ul className='navbar-nav offset-md-2 main-menu'>
+            <li>
+              <a href="link" className='active' >HOME</a>
+            </li>
+            <li>
+              <a href="link">CATÁLOGO</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <h1>O carro perfeito para você</h1>
+      </>
+  );
+};
+
+export default Navbar;
